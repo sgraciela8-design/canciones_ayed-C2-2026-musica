3063d92965c9701d07be16a30adf23c9ef2540c7
Graciela Adriana Segura <sgraciela8@gmail.com>
2026-09-08 13:23:40 -0300

    E1: agrego script de lista de canciones

--- esqueleto/src/dominio/lista_canciones.py
+++ esqueleto/src/dominio/lista_canciones.py
@@ -1,12 +1,23 @@
-import click
-import tabulate
-
-lista_canciones = [
-    {"id": 1, "Titulo": "Crimen", "Artista": "Gustavo Cerati","Album": "Fuerza natural"},
-    {"id": 2, "Titulo": "Adios", "Artista": "Gustavo Cerati", "Album": "Fuerza natural"},
-    {"id": 3, "Titulo": "Here Comes the Sun", "Artista": "The Beatles": "Album": "Abbey Road"},
+# Lista de canciones estructurada como un arreglo de diccionarios
+canciones = [
+    {"id": 1, "titulo": "Creep", "artista": "Radiohead", "album": "Pablo Honey", "anio": 1993},
+    {"id": 2, "titulo": "Karma Police", "artista": "Radiohead", "album": "OK Computer", "anio": 1997},
+    {"id": 3, "titulo": "Bohemian Rhapsody", "artista": "Queen", "album": "A Night at the Opera", "anio": 1975},
+    {"id": 4, "titulo": "Blinding Lights", "artista": "The Weeknd", "album": "After Hours", "anio": 2020},
+    {"id": 5, "titulo": "Hotel California", "artista": "Eagles", "album": "Hotel California", "anio": 1976}
 ]
 
-def listar_canciones():
-    for item in lista_canciones:
-        print (f"{item['id']: >2} {item ['Titulo']}")
+# Definición del formato de la tabla (los números definen el ancho de cada columna)
+# < indica alineación a la izquierda, > a la derecha
+formato_fila = "{:<4} | {:<20} | {:<15} | {:<22} | {:<14}"
+
+# 1. Imprimir la primera fila con los títulos de las columnas
+print(formato_fila.format("id", "titulo", "artista", "album", "año del album"))
+
+# Imprimir una línea divisoria estética
+print("-" * 85)
+
+# 2. Recorrer la lista e imprimir cada canción en formato tabla
+for c in canciones:
+    print(formato_fila.format(c["id"], c["titulo"], c["artista"], c["album"], c["anio"]))
+
